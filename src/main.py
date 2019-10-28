f85400392ba0b3ce1f0e433f9f81c0bb5e8f6b58
Wagner Santos <wosantos@thoughtworks.com>
2019-10-28 18:47:42 -0300

    Finished

--- src/main.py
+++ src/main.py
@@ -76,23 +76,20 @@ if __name__ == "__main__":
         generate_key_pair(bits_size)
 
     if (option == 2):
-        print("Encrypt a message")
-        # message = input("Enter the message: ")
-        message = 'heello wagner'
-        # public_key = input("Enter the public key: ")
-        public_key = 'MIICCQKCAQEAsaPaCCG28K7GrvGW4uSNB3iKBk3VrdGUX14JgRzBFCkGxFGvlGzTiw8aspsQcqXhjMova2qYU2pU3sBgsBSFoIZ5fkKbMrPT2w7DQgGJZcbosAPtlAKtAJ1ZN/V9snYhZCIC+pI+pNnAzxWgCVOGEvcYD0TpU+a/Q2C3EeS4eQwafXsksFwY7ZlyJHz+6PiE0hF9JUGeINYuvzzKv+4mFhwT4PkyZ1RREhJHJGSURqjicAIQ7brDbVettgRxmuPrrjo1rXBSAWL46NwBNnRmD72Jm9XXP/Y/xlzNstYl1BdDlkdiB8jJUlu0Cce2UDuMy6+O0jPpyQxaPHS8LVFZbwKCAQBqwN4H256s8dawHOuVEylPTPb8mHW9XFmFaI07PjMBjd9igynobopn9I6e61F6i/K4gxFE2cAl9OZr780yyPdYGUKa362hwADMSmGY7o/2gwQ/WJmDMq0bU+hOyE4ZgNRbaG4DQ3U0ulEZ6aaOWhqeyzGtHm93uTu+SJbIN7fH/z+wFm2AApkcB+oy23BPIBTM2fZ7FHUxL/T4VOQbk/mcyZNbuedCJ8/atnl88PPyjG8Ow65lQz5XlyxrTn6svjuVfasbwNjMWLhYoFEr3z+xWuwMzV+EhdBca6krCY/hPN/8RQbeEfZHCF4D8paIZ5oFoAG9fU5yoSfzOa44KQK/'
+        print("Encrypt a message...")
+        message = input("Enter the message: ")
+        public_key = input("Enter the public key: ")
         decode_key = decode_public_key(public_key)
 
         encrypt_message = encrypt(message, decode_key)
-        print(''.join(map(lambda x: str(x), encrypt_message)))
+        print(encrypt_message)
 
     if(option == 3):
         print("Decrypt a message")
-        # message = input("Enter the cipher message: ")
-        message = 'heello wagner'
-        # private_key = input("Enter the private key: ")
-        private_key = 'MIIFogIBAAKCAQEAsaPaCCG28K7GrvGW4uSNB3iKBk3VrdGUX14JgRzBFCkGxFGvlGzTiw8aspsQcqXhjMova2qYU2pU3sBgsBSFoIZ5fkKbMrPT2w7DQgGJZcbosAPtlAKtAJ1ZN/V9snYhZCIC+pI+pNnAzxWgCVOGEvcYD0TpU+a/Q2C3EeS4eQwafXsksFwY7ZlyJHz+6PiE0hF9JUGeINYuvzzKv+4mFhwT4PkyZ1RREhJHJGSURqjicAIQ7brDbVettgRxmuPrrjo1rXBSAWL46NwBNnRmD72Jm9XXP/Y/xlzNstYl1BdDlkdiB8jJUlu0Cce2UDuMy6+O0jPpyQxaPHS8LVFZbwKCAQBqwN4H256s8dawHOuVEylPTPb8mHW9XFmFaI07PjMBjd9igynobopn9I6e61F6i/K4gxFE2cAl9OZr780yyPdYGUKa362hwADMSmGY7o/2gwQ/WJmDMq0bU+hOyE4ZgNRbaG4DQ3U0ulEZ6aaOWhqeyzGtHm93uTu+SJbIN7fH/z+wFm2AApkcB+oy23BPIBTM2fZ7FHUxL/T4VOQbk/mcyZNbuedCJ8/atnl88PPyjG8Ow65lQz5XlyxrTn6svjuVfasbwNjMWLhYoFEr3z+xWuwMzV+EhdBca6krCY/hPN/8RQbeEfZHCF4D8paIZ5oFoAG9fU5yoSfzOa44KQK/AoIBABM6BIoMbGtUbryj/ZA8rHgQw89lTtnND4DPz7mUMQPIx5NT/1Lqnf85f/i68e+IdHQ03nJXD5Dy0+iCpDG7yviQNM3j7FsWT3yW7OKqPvMnE2L3rXKIx8qoTXsF/Bc0Huicd5ppuQZMt082+zYIjgeEnvX7ZYPkz7qsvoln/RCSIsc+ZFTmIf7HuWFevyS9JnJbCyWoLojk1OKno4ihIm+YML9xbHM6reJyK7WxzvTktC8g0hdoslG5pITwYy2Ain8RrKFZubEnlwqKFnvM+Ez29Nmod1Kd4jm8IqDWdOgmTJotfwsjQHa72nEH7F04Kl/rkc/xyWWfHUb94R0D4fcCgYEAvYCwYWRZdX6HWDsBQmwEbaP+881rDL6ACkWkH0zsULy0ztdqPM808RCUo2z2jMy/EUFhPR4HP9vkHnCih9FFlb3YOktLBAhK+9+0AIUp31jXf8MvnX52UkLN01Grg4VsOC33YEMSFVT7Jf0C7QB2C5K9J2BsCBN8UQ2xenDrKG0CgYEA7/mGAGPvNNF8d0nxNiUDWkzthDLKyThjjJC0mbqDgfDRPDqH8bBqkmgH4DPnKYpRCKwm0wiNSgs5ErNrJ1EM9p1MMlhkdA1STF5KgMi+olPZuguOV+6IH5xNcx9MgGX4NzWCJ4XcBvtUPJzS4vs1x7oP+yk7ALcT+cV9mOYAl8sCgYB6w2dJH1OPgr49fcatCZxL5d3t42knIjZp/+2Sy77oDce1cj9y450jSvkAVTf9JbyG4I84jy3mmgt2+cW5BqqzySoYbSmhMizI26vXDZbn+gL/3CicObs6qWQXMFGmMu/BMIIoO+Pf7xIp6Ez/rcj+ut3/l36CLLsjseRxcIXu9wKBgGZfkAE7MNyX60/QO7HXXFGRJaHmZFOGiIfcgQg1bVjPIZUdUO60i85kfxpIiaj3j8yXtSKi9dtKkiz9hPT1wgkuPXHzkhxq2A+JMSWGh8P3DHQUnrZD+Ya4LnwlUIEPrNpj3E+yaifExbDe9d/7YcqYQ+U3+zZnpHJnA7u2xY6nAoGBAIsH2sJkw7YrkjksEU6zBYD7EGNoC1BEnIf6k6TfVR+ImGF0TIft/0+5IWamBfAPLRnWm6czgTWsjyot2ehRfjTeZEI+MZQDQ6thHYBBlRxd1UV60OMOZITpTjOECoak4DkmbJkASCOuog9dMvcFtk9ralOXnQ9v5KhV5Vv71bkO'
+        message = input("Enter the cipher message: ")
+        private_key = input("Enter the private key: ")
 
         decode_key = decode_private_key(private_key)
 
-        message = decrypt(message, decode_key)
+        decoded_message = decrypt(int(message), decode_key)
+        print("Message: ", decoded_message)
